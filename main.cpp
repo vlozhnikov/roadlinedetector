15fd97f6e00751db2643981cb23efed531ddbc65
vladimir.lozhnikov <vladimir.lozhnikov@gmail.com>
2023-10-10 18:01:26 +0300

    a

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@
 
 #include <iostream>
 #include <numeric>
+#include <algorithm>
 
 using namespace cv;
 using namespace std;
 
 const Mat process(const Mat& mat);
+const std::map<int, std::vector<cv::Vec4i>> groupingLines(const std::vector<cv::Vec4i>& lines, const unsigned int count = 1);
+const std::pair<cv::Point, cv::Point> pointsFromLine(const cv::Vec4i& line, const float kOffset = 0.2);
+int distance(const Point& x1y1, const Point& x2y2);
 
 int main()
 {
@@ -117,12 +121,174 @@ const Mat process(const Mat& origin) {
         return (abs(degree) > 15);
     });
 
-    for (auto l : filteredLines) {
-        auto p1 = Point(l[0], l[1]);
-        auto p2 = Point(l[2], l[3]);
+    auto groupedBy = groupingLines(filteredLines, 10);
 
-        line(origin, p1, p2, Scalar(0, 0, 255), 2);
+//    for (const auto& e : groupedBy) {
+//        auto a = e.second;
+//        for (auto v : a) {
+//            std::cout << v << ", ";
+//        }
+//        std::cout << endl;
+//    }
+
+    // из каждой объединенной группы получить среднюю линию
+
+    for (const auto& e : groupedBy) {
+
+        auto v = e.second;
+
+        for (auto l : v) {
+
+            if (v.size() < 2) continue;
+
+            auto p1 = Point(l[0], l[1]);
+            auto p2 = Point(l[2], l[3]);
+
+            line(origin, p1, p2, Scalar(0, 255, 0), 5);
+            break;
+        }
     }
 
     return aoi;
+}
+
+const std::map<int, std::vector<cv::Vec4i>> groupingLines(const std::vector<cv::Vec4i>& lines, const unsigned int count) {
+
+    auto localCount = count;
+    if (localCount < 1) return {};
+
+    auto first = true;
+    std::vector<cv::Vec4i> localLines;
+
+    while (localCount > 0) {
+
+        std::map<int, std::vector<cv::Vec4i>> groupedBy;
+
+        if (first) {
+            for (const auto& e : lines) {
+                localLines.push_back(e);
+            }
+        }
+        first = false;
+
+        // сгруппировать линии по дельте (расстоянию) между ними
+        using groups = std::map<int, std::vector<cv::Vec4i>>;
+        groupedBy = std::reduce(localLines.begin(), localLines.end(), groups{}, [](groups acc, const cv::Vec4i& item) {
+
+    //        std::cout << endl << item << endl;
+
+            // find all deltas with grouped lines before
+            if (acc.empty()) {
+                acc[1] = std::vector<cv::Vec4i>{item}; // first index is 1
+                return acc;
+            }
+
+            // получить все ключи
+            auto keys = std::vector<int>();
+            for (const auto& [key, _] : acc) {
+                keys.push_back(key);
+            }
+
+            // получить максимальный ключ
+            auto maxKey = *std::max_element(keys.begin(), keys.end());
+
+            // получить лве точки линии
+            auto itemPoints = pointsFromLine(item);
+
+            for (const auto& key : keys) {
+                auto deltas = std::vector<int>();
+
+                // вычислить расстояния между item и line в двух точках
+                auto foundLines = std::vector<cv::Vec4i>();
+
+                std::copy_if(acc[key].begin(), acc[key].end(), std::back_inserter(foundLines), [itemPoints](const auto& line) {
+
+                    // получить две точки
+                    auto midPoints = pointsFromLine(line);
+
+                    // вычислить расстояния
+                    const auto delta = 5;
+
+                    auto firstDelta = false;
+                    auto secondDelta = false;
+
+                    auto d1 = distance(itemPoints.first, midPoints.first);
+                    if (d1 <= delta) firstDelta = true;
+                    auto d2 = distance(itemPoints.first, midPoints.second);
+                    if (d2 <= delta) {
+                        if (firstDelta) secondDelta = true;
+                        else firstDelta = true;
+                    }
+                    auto d3 = distance(itemPoints.second, midPoints.first);
+                    if (d3 <= delta) {
+                        if (firstDelta) secondDelta = true;
+                        else firstDelta = true;
+                    }
+                    auto d4 = distance(itemPoints.second, midPoints.second);
+                    if (d4 <= delta) {
+                        if (firstDelta) secondDelta = true;
+                        else firstDelta = true;
+                    }
+
+                    return firstDelta && secondDelta;
+                });
+
+    //            std::for_each(foundLines.begin(), foundLines.end(), [](const auto &e) {
+    //                std::cout << "found line: " << e << endl;
+    //            });
+
+                // если результат пусто, то создаем новую группу с новым ключем
+                if (foundLines.empty()) {
+                    acc[maxKey+1] = std::vector<cv::Vec4i>{item};
+                }
+                // иначе, добавляем текущую линию к найденной группе
+                else {
+                    acc[key].push_back(item);
+                }
+            }
+
+            return acc;
+        });
+
+        std::cout << "localLines size: " << localLines.size() << endl;
+
+        if (localLines.empty()) {
+            return groupedBy;
+        }
+
+        localLines.erase(localLines.begin(), localLines.end());
+        for (const auto& e : groupedBy) {
+            for (const auto& v : e.second) {
+                localLines.push_back(v);
+            }
+        }
+
+        localCount -= 1;
+        if (localCount == 0) {
+            return  groupedBy;
+        }
+    };
+
+    return {};
+}
+
+// получить две точки на отрезке с заданным смещением kOffset, относительно краев
+const std::pair<cv::Point, cv::Point> pointsFromLine(const cv::Vec4i& line, const float kOffset) {
+    auto xa = line[0];
+    auto ya = line[1];
+    auto xb = line[2];
+    auto yb = line[3];
+
+    auto x1 = xa + (xb - xa) * kOffset;
+    auto y1 = ya + (yb - ya) * kOffset;
+
+    auto x2 = xa + (xb - xa) * (1 - kOffset);
+    auto y2 = ya + (yb - ya) * (1 - kOffset);
+
+    return std::pair<cv::Point, cv::Point>{Point(x1, y1), Point(x2, y2)};
+}
+
+// вычислить расстояние между двумя точками
+int distance(const Point& x1y1, const Point& x2y2) {
+    return abs(sqrt(pow(x1y1.x - x2y2.x, 2) + pow(x1y1.y - x2y2.y, 2)));
 }
